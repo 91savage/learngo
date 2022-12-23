b637da6235d3227ad47b959572b47efb14582991
91savage <ekdma1403@gmail.com>
2022-12-23 17:52:49 +0900

    naked function + defer

--- main.go
+++ main.go
@@ -5,15 +5,37 @@ import (
 	"strings"
 )
 
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
+// func repeatMe(words ...string) {
+// 	fmt.Println(words)
+
+// }
+
+// naked return 
+func lenAndUpper(name string) (length int, uppercase string) {
+	// defer = return 이 끝나고 실행 됨.
+	defer fmt.Println("I'm Done")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+		return
+	// return length, uppercase 
 }
 
-func repeatMe(words ...string) {
-	fmt.Println(words)
+// func lenAndUpper(name string) (int, string) {
+//   // multi 값 return
+// 	return len(name), strings.ToUpper(name)
+// }
 
-}
+
+func multiply(a ,b int) int {  // 마지막 int는 return 할 값의 Type
+	return a * b
+}	
+
+// func main() {
+// 	repeatMe("nico", "peter", "Merry", "rudolph", "Saavy")
+// }
 
 func main() {
-	repeatMe("sehun","jins","kopiko","eclipse","TWG")
+	totalLength, up := lenAndUpper("Savage")
+	fmt.Println(totalLength, up)
+	
 }
