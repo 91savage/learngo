922c6ac0fe32b987f648ed4389d6e5bac8d25297
91savage <ekdma1403@gmail.com>
2023-01-06 11:53:54 +0900

    range = for 같음

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 // func repeatMe(words ...string) {
@@ -11,13 +10,40 @@ import (
 // }
 
 // naked return 
-func lenAndUpper(name string) (length int, uppercase string) {
-	// defer = return 이 끝나고 실행 됨.
-	defer fmt.Println("I'm Done")
-	length = len(name)
-	uppercase = strings.ToUpper(name)
-		return
-	// return length, uppercase 
+// func lenAndUpper(name string) (length int, uppercase string) {
+// 	// defer = return 이 끝나고 실행 됨.
+// 	defer fmt.Println("I'm Done")
+// 	length = len(name)
+// 	uppercase = strings.ToUpper(name)
+// 		return
+// 	// return length, uppercase 
+// }
+
+// func superAdd(numbers ...int) int {
+// 	for index, number := range numbers {
+// 		fmt.Println(index, number)
+// 	}
+// 	return 1
+// }
+
+// func superAdd(numbers ...int) int {
+// 	for i:=0; i< len(numbers); i++ {
+// 		fmt.Println(numbers[i])
+// 	}
+// 	return 1
+// }
+
+func superAdd(numbers ...int) int {
+	total := 0 
+	for _, number := range numbers {
+		total += number
+	}
+	return total
+}
+
+func main() {
+	result := superAdd(1,2,3,4,5,6)
+	fmt.Println(result)
 }
 
 // func lenAndUpper(name string) (int, string) {
@@ -26,16 +52,16 @@ func lenAndUpper(name string) (length int, uppercase string) {
 // }
 
 
-func multiply(a ,b int) int {  // 마지막 int는 return 할 값의 Type
-	return a * b
-}	
+// func multiply(a ,b int) int {  // 마지막 int는 return 할 값의 Type
+// 	return a * b
+// }	
 
 // func main() {
 // 	repeatMe("nico", "peter", "Merry", "rudolph", "Saavy")
 // }
 
-func main() {
-	totalLength, up := lenAndUpper("Savage")
-	fmt.Println(totalLength, up)
+// func main() {
+// 	totalLength, up := lenAndUpper("Savage")
+// 	fmt.Println(totalLength, up)
 	
-}
+// }
