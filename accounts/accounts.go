02fbb9fd4d492ffd51e11505b8906e5d297d082a
91savage <ekdma1403@gmail.com>
2023-01-16 18:01:30 +0900

    * pointer Deposit,Balance

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -10,6 +10,15 @@ type Account struct {
 func NewAccount(owner string) *Account{
 	account := Account{owner: owner, balance: 0}
 	return &account
-
+	// & : Address
+	// * : See trough
+}
+// Deposit x amount on your account
+func (a *Account) Deposit(amount int) {  // (a Account) Go에서 receiver 라고 함.
+	a.balance += amount
 }
 
+// Balance of your account
+func (a *Account) Balance() int {
+	return a.balance
+}
