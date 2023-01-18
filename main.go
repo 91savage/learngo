cdad2ce6dd4faf6187e52becfbd41ea66626494e
91savage <ekdma1403@gmail.com>
2023-01-18 16:16:52 +0900

    add method

--- main.go
+++ main.go
@@ -6,12 +6,19 @@ import (
 )
 
 func main() {
-	dictionary := mydict.Dictionary{"first": "First Word"}
-	//fmt.Println(dictionary["first"])
-	definition, err := dictionary.Search("first")
+	dictionary := mydict.Dictionary{}
+
+	word := "hello"
+	definition := "Greeting"
+
+	err := dictionary.Add(word, definition)
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
 	}
+	hello,_:= dictionary.Search(word)
+	fmt.Println("found", word, "definition:", hello)
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
+}
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -8,6 +8,7 @@ import (
 type Dictionary map[string]string
 
 var errNotFound = errors.New("Not Found")
+var errWordExists = errors.New("That World already Exists")
 
 // Search for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -16,4 +17,16 @@ func (d Dictionary) Search(word string) (string, error) {
 		return value, nil
 	}
 	return "", errNotFound
+}
+
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case errNotFound:
+		d[word] =def
+	case nil:
+		return errWordExists
+	}
+	return nil
 }
