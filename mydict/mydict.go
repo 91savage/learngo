aab33c5591f2c6f44e4547c6f71cf4d6c0f8c2e9
91savage <ekdma1403@gmail.com>
2023-01-19 16:00:39 +0900

    Basic Function Complete

--- mydict/mydict.go
+++ mydict/mydict.go
@@ -7,8 +7,12 @@ import (
 // Dictionary Type
 type Dictionary map[string]string
 
-var errNotFound = errors.New("Not Found")
-var errWordExists = errors.New("That World already Exists")
+var (
+	errNotFound = errors.New("Not Found")
+	errorCantUpdate = errors.New("Cant Update non-existing Word")
+	errWordExists = errors.New("That World already Exists")
+)
+
 
 // Search for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -29,4 +33,22 @@ func (d Dictionary) Add(word, def string) error {
 		return errWordExists
 	}
 	return nil
+}
+
+// Update a word
+func (d Dictionary) Update(word, definition string) error{
+	_, err := d.Search(word)
+
+	switch err {
+	case nil:
+		d[word] = definition
+	case errNotFound:
+		return errorCantUpdate
+	}
+	return nil
+}
+
+// Delete a word
+func (d Dictionary) Delete(word string){
+	delete(d, word)
 }
