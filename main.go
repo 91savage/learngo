c9e5641542584c3238283a7765a85dc8e5bdcf9f
91savage <ekdma1403@gmail.com>
2023-02-02 14:42:06 +0900

    기사 긁어서 배열에 저장 완료

--- main.go
+++ main.go
@@ -5,11 +5,12 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-type extractJob struct {
+type extractedJob struct {
 	id      string
 	title   string
 	summary string
@@ -17,19 +18,22 @@ type extractJob struct {
 	writer  string
 }
 
-var baseURL string = "https://www.mbn.co.kr/news/politics/"
+var baseURL string = "https://www.mbn.co.kr/news/entertain/"
 
 func main() {
+	var jobs []extractedJob
 	totalPages := getPages() - 5 // MBN 페이징은 10까지만 돼있음 & 1page만 이상하게 tit 하위에 a href id 값이 다름. 그래서 2 page 부터 10page 까지만 진행함.
-	fmt.Println(totalPages)
 
 	for i := 2; i <= totalPages; i++ {
-		getPage(i)
+		extractedJobs := getPage(i)
+		jobs = append(jobs, extractedJobs...)
 	}
+	fmt.Println(jobs)
 }
 
-func getPage(page int) {
-	pageURL := baseURL + "?page=" + strconv.Itoa((page)) + "&vod=&category=politics" // Itoa 는 숫자를 텍스트로 변환
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
+	pageURL := baseURL + "?page=" + strconv.Itoa((page)) + "&vod=&category=entertain" // Itoa 는 숫자를 텍스트로 변환
 	fmt.Println("Requesting", pageURL)
 	res, err := http.Get(pageURL)
 	checkErr(err)
@@ -40,23 +44,40 @@ func getPage(page int) {
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
 
-	searchCards := doc.Find(".tit")
+	searchCards := doc.Find(".article_list")
 
 	searchCards.Each(func(i int, s *goquery.Selection) { // s means each cards
-		id, _ := s.Find("a").Attr("href")
-		// fmt.Println(exists)
-		fmt.Println(id)
-		// if exists {
-		// 	fmt.Println(id)
-		// }
-
+		job := extractJob(s)
+		jobs = append(jobs, job)
 	})
+	return jobs
+
+}
+
+func extractJob(s *goquery.Selection) extractedJob {
+	id, _ := s.Find(".tit>a").Attr("href")
+	title := cleanString(s.Find(".tit>a").Text())
+	summary := cleanString(s.Find(".desctxt>a").Text())
+	date := cleanString(s.Find(".date").Text())
+	writer := cleanString(s.Find(".name_b").Text())
+
+	return extractedJob{
+		id:      id,
+		title:   title,
+		summary: summary,
+		date:    date,
+		writer:  writer,
+	}
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 
 }
 
 func getPages() int {
 	pages := 0
-	res, err := http.Get(baseURL + "?page=1&vod=&category=politics")
+	res, err := http.Get(baseURL + "?page=2&vod=&category=entertain")
 	checkErr(err)
 	checkCode(res)
 	defer res.Body.Close()
